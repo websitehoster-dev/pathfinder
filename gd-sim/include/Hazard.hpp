9f252baab17b1840424886bec4c0196628df352f
websitehoster-dev <lardxbox2006@gmail.com>
2025-09-04 21:08:52 +0200

    Update Hazard.hpp

--- gd-sim/include/Hazard.hpp
+++ gd-sim/include/Hazard.hpp
@@ -3,7 +3,13 @@
 
 struct Hazard : public Object {
     Hazard(Vec2D size, std::unordered_map<int, std::string>&& fields);
+    bool touching(Player const&) const override;
     void collide(Player&) const override;
+    
+private:
+    bool pointInTriangle(Vec2D p, Vec2D a, Vec2D b, Vec2D c) const;
+    bool lineSegmentsIntersect(Vec2D p1, Vec2D p2, Vec2D p3, Vec2D p4) const;
+    float direction(Vec2D a, Vec2D b, Vec2D c) const;
 };
 
 struct Sawblade : public Hazard {
